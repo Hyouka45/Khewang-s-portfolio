46f52463689677f162b18584f808750e88b1e9fe
Hyouka45 <khewang212@gmail.com>
2024-12-12 19:47:23 +0545

    restore

--- src/main.jsx
+++ src/main.jsx
@@ -5,10 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename ={import.meta.env.BASE_URL}>
-    <Routes>
-      <Route path="*" element={<App />}/>
-    </Routes>
-    </BrowserRouter>
+    <App/>
   </StrictMode>,
 )
